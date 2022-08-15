b910ef15c730c02d512f024670dd6033e95fe661
Richa0043 <48256863+Richa0043@users.noreply.github.com>
2022-08-15 23:31:30 +0530

    Add files via upload

---
+++ speech.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  9 18:44:59 2019
+
+@author: Richa Rani
+"""
+
+import pyttsx3
+speaker=pyttsx3.init()
+speaker.say("hello shubham i love you my jaan")
+speaker.runAndWait()  
